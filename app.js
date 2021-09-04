3a8389a14cd35b8f401f3ad13ae96d35f6ac8540
stardoxx <ashutoshsinghyadav03@gmail.com>
2021-09-04 14:40:37 +0530

    handling errors

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
+const morgan  = require('morgan');
 
-
-
+//routes and their specific actions
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev')); //logging our actions
+
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//catching errors that have above routes
+ app.use((req, res, next) => {
+     const error = new Error('Not Found');
+     error.status = 404;
+     next(error);
+ })
+
+ //in case in future if we face any errors within our application
+ app.use((error, req, res, next) => {
+     //for errors like of database etc.
+     res.status(error.status || 500);
+     res.json({
+         error:{
+             message:error.message
+         }
+     });
+ });
+
 
 module.exports = app;
