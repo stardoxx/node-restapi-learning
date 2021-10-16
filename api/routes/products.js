099b9e8d42ae584f7e55ea184679f047cd5a4eea
stardoxx <ashutoshsinghyadav03@gmail.com>
2021-10-16 18:38:48 +0530

    connecting with mongoDB and mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,49 +1,138 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require("../models/product");
+
+
 
 router.get('/',(req, res) => {
-    res.status(200).json({
-        message:"Handling GET request to /products"
-    });
+    // res.status(200).json({
+    //     message:"Handling GET request to /products"
+    // });
+    Product.find().exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+        })
+    })
 });
 
 router.post('/',(req, res) => {
-    const product = {
+    // const product = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // };
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(200).json({
-        message:"Handling POST request to /products",
-        createdProduct: product
+    })
+    product.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message:"Handling POST request to /products",
+            createdProduct: product
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
+
+    // res.status(201).json({
+    //     message:"Handling POST request to /products",
+    //     createdProduct: product
+    // });
 });
 
 
 router.get('/:productId',(req, res) => {
     const id  = req.params.productId;
-    if(id === 'special'){
-        res.status(200).json({
-            message: "this is special product",
-            id: id
+    // if(id === 'special'){
+    //     res.status(200).json({
+    //         message: "this is special product",
+    //         id: id
+    //     });
+    // }
+    // else{
+    //     res.status(200).json({
+    //         message: `you passed on id ${id}`
+    //     });
+    // }
+    Product.findById(id).exec()
+    .then(doc => {
+        console.log(doc);
+        if(doc){
+            res.status(200).json(doc);
+        }
+        else{
+            res.status(404).json({
+                message: "No valid entry point found for provided id: ${id}"
+            });
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
         });
-    }
-    else{
-        res.status(200).json({
-            message: `you passed on id ${id}`
-        });
-    }
+    })
+
+
 });
 
 
 router.patch('/:productId',(req, res) => {
-    res.status(200).json({
-        message: "updated product!"
-    });
+    // res.status(200).json({
+    //     message: "updated product!"
+    // });
+    const id = req.params.productId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({_id: id}, {
+        $set: updateOps,
+    })
+    .exec()
+    .then(result =>{
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
+
 });
 
 router.delete('/:productId',(req, res) => {
-    res.status(200).json({
-        message: "deleted product"
+    // res.status(200).json({
+    //     message: "deleted product"
+    // });
+    const id = req.params.productId;
+    Product.remove({_id: id})
+    .exec()
+    .then(result =>{
+        res.status(200).json(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err});
     });
 });
 
--- app.js
+++ app.js
@@ -2,23 +2,29 @@ const express = require('express');
 const app = express();
 const morgan  = require('morgan');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 
 
 //routes and their specific actions
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+// connecting with mongoose
+mongoose.connect('mongodb://localhost:27017/restapi_db');
+
+
+
 
 app.use(morgan('dev')); //logging our actions
 app.use(bodyParser.urlencoded({extended: false})); 
 app.use(bodyParser.json()); 
-
+//for checking headers.
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
+    //browsers send options request first in post and patch requests.
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 
             "PUT, POST, DELETE, PATCH, GET"
@@ -30,7 +36,7 @@ app.use((req, res, next) => {
 
 
 
-
+//connecting headers with their respective api routes.
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
